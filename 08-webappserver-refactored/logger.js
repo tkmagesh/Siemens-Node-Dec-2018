d6be759ffc5a1b2ad5d3d36107b9da4b8fda7f67
Magesh <tkmagesh77@gmail.com>
2018-12-13 16:05:09 +0530

    Day-02 Complete

--- 08-webappserver-refactored/logger.js
+++ 08-webappserver-refactored/logger.js
@@ -1,9 +1,11 @@
+var chalk = require('chalk');
+
 module.exports = function(req, res, next){
 	var startTime = new Date();
 	res.on('finish', function(){
 		var endTime = new Date();
 		var delta = endTime - startTime;
-		console.log(req.method + '\t' + req.urlObj.pathname + '\t' + delta + 'ms' + '\t' + res.statusCode);
+		console.log(chalk.red(req.method) + '\t' + chalk.blue(req.urlObj.pathname) + '\t' + chalk.bgGreen(delta + 'ms') + '\t' + chalk.cyan(res.statusCode));
 	});
 	next();
 };
