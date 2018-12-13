e8961482f6e1ea5d8ceff7d883d380dafda3072b
Magesh <tkmagesh77@gmail.com>
2018-12-13 13:42:08 +0530

    introducing app

--- 08-webappserver-refactored/server.js
+++ 08-webappserver-refactored/server.js
@@ -1,26 +1,17 @@
 var http = require('http');
 
-var dataParser = require('./dataParser'),
+var app = require('./app');
+	dataParser = require('./dataParser'),
 	serveStatic = require('./serveStatic'),
 	serveCalculator = require('./serveCalculator'),
 	notFoundHandler = require('./notFoundHandler');
 
-var _middlewares = [dataParser, serveStatic, serveCalculator, notFoundHandler];
+app.use(dataParser)
+app.use(serveStatic)
+app.use(serveCalculator)
+app.use(notFoundHandler);
 
-function exec(req, res, middlewares){
-	var first = middlewares[0],
-		remaining = middlewares.slice(1),
-		next = function(){
-			exec(req, res, remaining);
-		};
-	if (typeof first === 'function')
-		first(req, res, next);
-}
-
-var server = http.createServer(function(req, res,){
-	console.log(req.method + '\t' + req.url);
-	exec(req, res, _middlewares);	
-});
+var server = http.createServer(app);
 
 server.listen(8080);
 
