6add0824fe25905bc17bf8a4f847285d78d1139f
Magesh <tkmagesh77@gmail.com>
2018-12-13 10:04:27 +0530

    handling post requests

--- 05-webServer/server.js
+++ 05-webServer/server.js
@@ -20,7 +20,7 @@ var server = http.createServer(function(req /* ReadableStream */, res /* Writabl
 server.listen(8080);
 
 server.on('listening', function(){
-	console.log('server listening on 8080!!');
+	console.log('web server listening on 8080!!');
 });
 
 console.log('starting server...');
--- 06-appserver/server.js
+++ 06-appserver/server.js
@@ -6,7 +6,7 @@ var calculator = require('./calculator');
 
 var server = http.createServer(function(req, res,){
 	var urlObj = url.parse(req.url);
-	if (urlObj.pathname === '/calculator'){
+	if (urlObj.pathname === '/calculator' && req.method === 'GET'){
 		var data = querystring.parse(urlObj.query),
 			op = data.op,
 			x = parseInt(data.x),
@@ -15,14 +15,29 @@ var server = http.createServer(function(req, res,){
 		var result = calculator[op](x,y);
 		res.write(result.toString());
 		res.end();
+	} if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+		var rawData = '';
+		req.on('data', function(chunk){
+			rawData += chunk;
+		});
+		req.on('end', function(){
+			var data = querystring.parse(rawData),
+				op = data.op,
+				x = parseInt(data.x),
+				y = parseInt(data.y);
+
+			var result = calculator[op](x,y);
+			res.write(result.toString());
+			res.end();	
+		});
 	} else {
 		res.statusCode = 404;
 		res.end();
 	}
 });
 
-server.listen(8080);
+server.listen(8085);
 
 server.on('listening', function(){
-	console.log('server listening on 8080');
+	console.log('app server listening on 8085');
 });
